b3ac65e72fb5b6a9deace24ac82abacff3f7b537
KaioPimentel8 <159165485+KaioPimentel8@users.noreply.github.com>
2024-10-02 08:56:03 -0300

    spotify

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import conectaNaDb from './db.js';
 import cors from 'cors';
+import Artista from './Models/Artista.js'
 
 const app = express();
 app.use(cors());
@@ -12,6 +13,11 @@ conexao.on('error', (error) =>{
 
 conexao.once('open', () => {
     console.log('Conectado ao MongoDb')
+})
+
+app.get('/artistas', async (req, res) => {
+    const listaArtistas = await Artista.find({});
+    res.status(200).json(listaArtistas);
 })
 
 app.listen(3000, () => {
--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export default async function conectaNaDb() {
-    mongoose.connect("mongodb+srv://kaio:123@cluster0.muwsh.mongodb.net/spotify?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect("mongodb+srv://kaio:kaio@cluster0.muwsh.mongodb.net/spotify?retryWrites=true&w=majority&appName=Cluster0")
 
     return mongoose.connection;
 }
