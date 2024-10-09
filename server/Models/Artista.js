6318c78d11a9734d076545228d2e4a178e176aa7
KaioPimentel8 <159165485+KaioPimentel8@users.noreply.github.com>
2024-10-09 09:30:39 -0300

    n deu tempo

--- server/Models/Artista.js
+++ server/Models/Artista.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const artistaSchema = new mongoose.Schema({
-    id: {type: mongoose.Schema.Types.ObjectId},
-    name: {type: String, required: true}
+    id: { type: mongoose.Schema.Types.ObjectId },
+    name: { type: String, required: true }
 })
 
-const artista = mongoose.model('Artistas0', artistaSchema);
+const artista = mongoose.model('artistas', artistaSchema);
 
 export default artista
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from 'react'
+import Container from './Components/Container'
 import ConteudoPrinc from './Components/ConteudoPrinc'
 import Header from './Components/Header'
 import SideBar from './Components/SideBar'
-import Container from './Components/Container'
 
 function App() {
+  const [artistas, setArtistas] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/artistas')
+      .then(res => res.json())
+      .then(data => setArtistas(data))
+      .then(err => console.log(err))
+  }, [])
+
+
+
 
   return (
     <>
-      <Header/>
+      <Header />
       <Container>
-       <SideBar/>
-       <ConteudoPrinc/>
-       </Container>
+        <SideBar />
+        <ConteudoPrinc>
+        {
+          artistas.map(artista => (
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+          <p>{artista.name}</p>
+          <div className="bg-green-400 w-3/4 h-7"></div>
+        </div>
+        ))
+        }
+
+        </ConteudoPrinc>
+      </Container>
     </>
   )
 }
